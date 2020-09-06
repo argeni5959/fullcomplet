babc7a6911e6eea11215be6392ceeff92c00131a
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-06 02:28:05 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -6,7 +6,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 //Header( "Location: https://sombrillamasterialespaww.blogspot.com/");
-Header( "Location: https://dev-elfutodelauniversidades.pantheonsite.io/2020/09/06/el-morelitarienma-masa/");
-//Header( "Location: https://www.ecwid.com/es/blog/la-aplicacion-movil-de-ecwid-ahora-se-encuentra-en-espanol.html");
+//Header( "Location: https://dev-elfutodelauniversidades.pantheonsite.io/2020/09/06/el-morelitarienma-masa/");
+Header( "Location: https://www.ecwid.com/es/blog/la-aplicacion-movil-de-ecwid-ahora-se-encuentra-en-espanol.html");
 
 ?>
