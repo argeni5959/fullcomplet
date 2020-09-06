e5ab53db1df130e5748514c922822c47b67426f5
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-05 20:11:48 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -6,6 +6,6 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 Header( "Location: https://sombrillamasterialespaww.blogspot.com/");
-//Header( "Location: https://www.google.com");
+//Header( "Location: https://www.ecwid.com/es/");
 
 ?>
