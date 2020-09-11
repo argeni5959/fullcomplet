2e635f7891f98201d75a18abcf9f1aea3d0b1b63
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-11 18:00:14 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -5,7 +5,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 
-Header( "Location: https://drive.google.com/file/d/1xpMCeyN-UDWKjMpy75a5tynjAKFwrYf2/view");
+Header( "Location: https://drive.google.com/file/d/1xYaLKH_kvUdi4nryRySO9s7kqmt2ZLFk/view");
 
 //Header( "Location: https://google.com");
 
