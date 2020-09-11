cdabdda2b2f0aa1bb762a44a77ca153a161b7620
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-11 10:43:58 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -5,8 +5,8 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 
-//Header( "Location: https://listenermantrial.blogspot.com/");
+Header( "Location: https://listenermantrial.blogspot.com/");
 
-Header( "Location: https://google.com");
+//Header( "Location: https://google.com");
 
 ?>
