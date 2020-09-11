f436056d3008618582d26d15e659ae2b570bbe82
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-11 09:26:28 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -5,7 +5,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 
 
-Header( "Location: https://spawrrowark.blogspot.com/");
+Header( "Location: https://listenermantrial.blogspot.com/");
 
 //Header( "Location: https://google.com");
 
